c10d00d502b7ba4fb232c1cdb19a4bb7b2a67a27
Maxime Beauchemin <maxbeauchemin@gmail.com>
2015-09-30 13:59:16 -0500

    Created Database Helper Class
    The database helper is an object which helps you connect to the database
    and perform a particular call in a simple and user friendly way. The
    class supports Select, Update, Insert, and Delete calls takes as
    argument the database connection string.
    
    Also added references to the project for MySQL client, and
    System.Configuration to allow App.Config reading of the connection
    string

---
+++ OffCampusHousingDatabase/OffCampusHousingDatabase/DatabaseHelper.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace OffCampusHousingDatabase
+{
+    class DatabaseHelper
+    {
+        String connectionString;
+
+        public DatabaseHelper(String connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public ArrayList DatabaseSelect(String tableName)
+        {
+            return DatabaseSelect(tableName, "");
+        }
+
+        public ArrayList DatabaseSelect(String tableName, String whereClause)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            String sql = "SELECT * FROM `" + tableName + "` ";
+
+            if (!whereClause.Equals(""))
+            {
+                sql += " WHERE " + whereClause;
+            }
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            //number of columns
+            int numCols = reader.FieldCount;
+
+            ArrayList output = new ArrayList();
+
+            while (reader.Read())
+            {
+                String[] row = new String[numCols];
+                for (int i = 0; i < numCols; i++)
+                {
+                    row[i] = (reader.GetString(i));
+                }
+                output.Add(row);
+            }
+
+            conn.Close();
+
+            return output;
+        }
+
+        public void DatabaseInsert(String tableName, String Columns, String values)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            String sql = "INSERT INTO `" + tableName + "`(" + Columns + ") VALUES (" + values + ")";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            cmd.ExecuteReader();
+            conn.Close();
+        }
+
+        public void DatabaseUpdate(String tableName, String updateColumns, String whereClause)
+        {
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            String sql = "UPDATE `" + tableName + "` SET " + updateColumns + " WHERE " + whereClause;
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            cmd.ExecuteReader();
+            conn.Close();
+        }
+
+        private void DatabaseDelete(String tableName, String whereClause)
+        {
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            conn.Open();
+
+            String sql = "DELETE FROM `" + tableName + "` WHERE " + whereClause;
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            cmd.ExecuteReader();
+            conn.Close();
+        }
+    }
+}
